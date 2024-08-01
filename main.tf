efa8a5ad36e9e2646f6a346cbf8300fa5d996437
Shahid Mahmood <shahidmahmood.yar@gmail.com>
2024-08-01 16:36:27 +0500

    terraform script without source trigger

---
+++ main.tf
@@ -0,0 +1,222 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "dev"
+}
+
+resource "aws_codepipeline" "codepipeline" {
+  name     = "tf-poc-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  pipeline_type = "V2"
+  execution_mode = "PARALLEL"
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+      configuration = {
+        ConnectionArn    = var.existing_connection_arn
+        FullRepositoryId = "imshahidmahmood/deploy-backend-cdk-improved"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "CodeBuild"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+      configuration = {
+        ProjectName = aws_codebuild_project.build_project.name
+      }
+    }
+  }
+}
+
+resource "aws_codepipeline_webhook" "source_webhook" {
+  name            = "source-webhook"
+  target_action   = "Source"
+  target_pipeline = aws_codepipeline.codepipeline.name
+  authentication  = "GITHUB_HMAC"
+  authentication_configuration {
+    secret_token = var.github_token
+  }
+  filter {
+    json_path    = "$.ref"
+    match_equals = "refs/tags/tf-*"
+  }
+}
+
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "tf-poc-codepipeline-bucket-1234"
+}
+
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "codepipeline.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name   = "codepipeline-policy"
+  role   = aws_iam_role.codepipeline_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          aws_s3_bucket.codepipeline_bucket.arn,
+          "${aws_s3_bucket.codepipeline_bucket.arn}/*"
+        ]
+      },
+      {
+        Action = [
+          "codebuild:BatchGetBuilds",
+          "codebuild:StartBuild"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          aws_codebuild_project.build_project.arn
+        ]
+      },
+      {
+        Action = [
+          "codestar-connections:UseConnection",
+          "codestar-connections:ListConnections"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          var.existing_connection_arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_codebuild_project" "build_project" {
+  name          = "codebuild-project"
+  description   = "CodeBuild project for CodePipeline"
+  build_timeout = "5"
+
+  service_role = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/amazonlinux2-x86_64-standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    environment_variable {
+      name  = "EXAMPLE_ENV_VAR"
+      value = "example-value"
+    }
+  }
+
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "codebuild.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "codebuild_policy" {
+  name   = "codebuild-policy"
+  role   = aws_iam_role.codebuild_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_admin_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+variable "existing_connection_arn" {
+  description = "ARN of the existing CodeStar connection"
+  type        = string
+}
+
+variable "github_token" {
+  description = "OAuth token for GitHub"
+  type        = string
+  sensitive   = true
+}
